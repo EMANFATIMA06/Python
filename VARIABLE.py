4994765fdb3b522b6fe79983bb2fc9aa14e841a1
EMAN FATIMA <181399217+EMANFATIMA06@users.noreply.github.com>
2024-11-11 18:55:18 +0500

    Add files via upload

---
+++ VARIABLE.py
@@ -0,0 +1,27 @@
+
+# -----> Basic Variable Assignment
+
+value1 = 154
+value2 = "hello"
+value3 = 3.14
+value4 = True
+value5 = ["apple","mango","banana"]
+print("value1:",value1 ,"type:", type(value1))
+print("value2:",value2 ,"type:", type(value2))
+print("value3:",value3 ,"type:", type(value3))
+print("value4:",value4 ,"type:", type(value4))
+print("value5:",value5 ,"type:", type(value5))
+
+# -----> Swapping Variables
+
+a=12
+b=13
+a,b=b,a
+print("a:", a)
+print("b:", b)
+
+# -----> String Concatenation
+
+first_name = "eman"
+last_name = "fatima"
+print(first_name + last_name)
